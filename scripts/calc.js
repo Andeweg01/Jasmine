14c0337bfe21c5271066d7173c6f33d3ff31d238
Sascha Andeweg <sascha@designed.irish>
2020-01-11 20:05:20 +0000

    commit after adding defensive programming

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,7 @@
-function addition() {
-    return 42;
+function addition(numOne, numTwo) {
+    if(typeof(numOne) == "number" && typeof(numTwo) == "number") {
+        return numOne + numTwo;
+} else {
+    return "Error";
+}
 }

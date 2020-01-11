be34b1fc2cfabad1714f418eb050d01260501609
Sascha Andeweg <sascha@designed.irish>
2020-01-11 19:07:37 +0000

    commit after doing first simple test on returning 42

---
+++ spec/calcSpec.js
@@ -0,0 +1,7 @@
+describe("Calculator", function() {
+    describe("Addition tests", function() {
+        it("should return 42", function() {
+            expect(addition(20, 22)).toBe(42);
+        })
+    })
+})

260b4047916ffa68d4c263b50e004060b19d9218
Sascha Andeweg <sascha@designed.irish>
2020-01-12 16:27:49 +0000

    commit after finishing the test fixing video

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,7 +1,11 @@
-function addition(numOne, numTwo) {
-    if(typeof(numOne) == "number" && typeof(numTwo) == "number") {
-        return numOne + numTwo;
-} else {
-    return "Error";
+Calculator = function() {
+    this.value = 0;
 }
+
+Calculator.prototype.add = function(number) {
+    if(typeof(number) == "number") {
+        this.value += number;
+    } else {
+        alert("Error!");
+    }
 }
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,13 +1,24 @@
 describe("Calculator", function() {
+
+    beforeEach(function() {
+        calc = new Calculator;
+    });
+
     describe("Addition tests", function() {
         it("should return 42", function() {
-            expect(addition(20, 22)).toBe(42);
+            calc.add(20);
+            calc.add(22);
+            expect(calc.value).toBe(42);
         });
         it("should return 26", function() {
-            expect(addition(7, 19)).toBe(26);
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26);
         });
  		it("should return an error if we don't supply two numbers", function() {
-    		expect(addition("Hitchhikers", "Guide")).toBe("Error");
+            spyOn(window, "alert");
+            calc.add("Hitchhikers");
+            expect(window.alert).toHaveBeenCalledWith("Error!");
 		});
     });
 });
